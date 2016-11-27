b6edc3584e2226be59451f25f430473cdec01488
Jordan Scales <scalesjordan@gmail.com>
2016-11-27 12:23:30 -0500

    add --show option to cli

--- cli.js
+++ cli.js
@@ -1,8 +1,25 @@
-const { evaluate } = require("./")
+const { generateCode, parse, evaluate } = require("./")
+
+let showCode = false
+const showIndex = process.argv.findIndex(arg => arg === "--show")
+if (showIndex > -1) {
+    showCode = true
+    process.argv.splice(showIndex, 1)
+}
 
 if (process.argv.length < 3) {
-    console.log("Enter some code")
+    console.log("Usage: ./cli [--show] code")
     process.exit(1)
 } else {
-    evaluate(process.argv[2])
+    try {
+        const input = process.argv[2]
+        if (showCode) {
+            console.log(generateCode(parse(input)))
+            console.log("\n---\n")
+        }
+
+        evaluate(input)
+    } catch (e) {
+        console.log(e)
+    }
 }
